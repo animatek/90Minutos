a2f773ed07d7c2a88b761f2216087c3d26f6effb
animatek <animatek909@gmail.com>
2025-09-19 19:16:47 +0200

    k

--- scripts/start_dev_console.ps1
+++ scripts/start_dev_console.ps1
@@ -1,7 +1,7 @@
 # Abre una consola y ejecuta npm run dev en G:\Timer90
 Start-Process -FilePath "powershell.exe" `
-  -ArgumentList '-NoExit','-Command','cd ''G:\Timer90''; npm run dev' `
-  -WorkingDirectory 'G:\Timer90'
+  -ArgumentList '-NoExit','-Command','cd ''G:\90Minutos\''; npm run dev' `
+  -WorkingDirectory 'G:\90Minutos\'
   # Abre tu panel en el navegador si quieres (cambia el puerto)
 Start-Process 'http://127.0.0.1:5173'
 
